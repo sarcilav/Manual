bea37d45e5d3aa6c61cbc452167eafe00390f91b
Nicolás Hock Isaza <nhocki@gmail.com>
2010-09-16 19:42:36 -0500

    Adding suffix arrays

--- string_matching/kmp.cpp
+++ string_matching/kmp.cpp
@@ -4,13 +4,16 @@
 #include <string>
 #include <vector>
 #include <cassert>
+#include <set>
 
 using namespace std;
+
+#define foreach(x, v) for (typeof (v).begin() x = (v).begin(); x != (v).end(); ++x)
 
 // Computes the jumping function
 vector<int> kmp_table(string &P){
     int n = P.size();
-    vector<int> T(n);
+    vector<int> T(n+1);
     T[0] = -1; T[1] = 0;
     for(int pos = 2, cnd = 0; pos < n; ++pos){
 	if(P[pos -1] == P[cnd]){ T[pos] = ++cnd;}
@@ -22,24 +25,31 @@ vector<int> kmp_table(string &P){
 
 // Looking for W in S
 void kmp_search(string &S, string &W){
-    printf("Looking for  %s  in  %s\n", W.c_str(), S.c_str());
+    //printf("Looking for  %s  in  %s\n", W.c_str(), S.c_str());
+    set<int>s;
     int m = 0, i = 0, n = S.size(), w = W.size();
     vector<int> T = kmp_table(W);
     while( m + i < n){
 	if(W[i] == S[m+i]){
-	    if(++i == w) printf("There's a match at %d\n", m);
+	    if(++i == w){s.insert(m); m--; }//printf("There's a match at %d\n", m); m--;}
 	}
 	else{
 	    m += i - T[i];
 	    i = (T[i] > -1) ? T[i] : 0;
 	}
     }
+    printf("%d\n", s.size());
+    foreach(i,s){ printf("%d ", *(i)); }
+    puts("");
     return ;
 }
 
 int main(){
-    string T = "cadabrabracadabracadabra";
-    string P = "cadabra";
+    //string T = "CABBCABABABA";
+    //string P = "ABA";
     //string P = "ababababca";
+    string T,P;
+    cin >> P >> T;
+    kmp_search(T, P);
     return 0;
 }
---
+++ string_matching/suffix_arrays.cpp
@@ -0,0 +1,228 @@
+using namespace std;
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <climits>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+
+template <class T> string toStr(const T &x){ stringstream s; s << x; return s.str(); }
+template <class T> int toInt(const T &x){ stringstream s; s << x; int r; s >> r; return r; }
+
+#define For(i, a, b) for (int i=(a); i<(b); ++i)
+#define foreach(x, v) for (typeof (v).begin() x = (v).begin(); x != (v).end(); ++x)
+#define D(x) cout << #x " = " << (x) << endl
+
+const int N = 100005;
+// Begins Suffix Arrays implementation
+// O(n log n) - Manber and Myers algorithm
+
+//Usage:
+// Fill str with the characters of the string.
+// Call SuffixSort(n), where n is the length of the string stored in str.
+// That's it!
+
+//Output:
+// pos = The suffix array. Contains the n suffixes of str sorted in lexicographical order.
+//       Each suffix is represented as a single integer (the position of str where it starts).
+// rank = The inverse of the suffix array. rank[i] = the index of the suffix str[i..n)
+//        in the pos array. (In other words, pos[i] = k <==> rank[k] = i)
+//        With this array, you can compare two suffixes in O(1): Suffix str[i..n) is smaller
+//        than str[j..n) if and only if rank[i] < rank[j]
+
+int str[N]; //input
+int rank[N], pos[N]; //output
+int cnt[N], next[N]; //internal
+bool bh[N], b2h[N];
+
+bool smaller_first_char(int a, int b){
+  return str[a] < str[b];
+}
+
+void SuffixSort(int n){
+  //sort suffixes according to their first character
+  for (int i=0; i<n; ++i){
+    pos[i] = i;
+  }
+  sort(pos, pos + n, smaller_first_char);
+  //{pos contains the list of suffixes sorted by their first character}
+
+  for (int i=0; i<n; ++i){
+    bh[i] = i == 0 || str[pos[i]] != str[pos[i-1]];
+    b2h[i] = false;
+  }
+
+  for (int h = 1; h < n; h <<= 1){
+    //{bh[i] == false if the first h characters of pos[i-1] == the first h characters of pos[i]}
+    int buckets = 0;
+    for (int i=0, j; i < n; i = j){
+      j = i + 1;
+      while (j < n && !bh[j]) j++;
+      next[i] = j;
+      buckets++;
+    }
+    if (buckets == n) break; // We are done! Lucky bastards!
+    //{suffixes are separted in buckets containing strings starting with the same h characters}
+
+    for (int i = 0; i < n; i = next[i]){
+      cnt[i] = 0;
+      for (int j = i; j < next[i]; ++j){
+        rank[pos[j]] = i;
+      }
+    }
+
+    cnt[rank[n - h]]++;
+    b2h[rank[n - h]] = true;
+    for (int i = 0; i < n; i = next[i]){
+      for (int j = i; j < next[i]; ++j){
+        int s = pos[j] - h;
+        if (s >= 0){
+          int head = rank[s];
+          rank[s] = head + cnt[head]++;
+          b2h[rank[s]] = true;
+        }
+      }
+      for (int j = i; j < next[i]; ++j){
+        int s = pos[j] - h;
+        if (s >= 0 && b2h[rank[s]]){
+          for (int k = rank[s]+1; !bh[k] && b2h[k]; k++) b2h[k] = false;
+        }
+      }
+    }
+    for (int i=0; i<n; ++i){
+      pos[rank[i]] = i;
+      bh[i] |= b2h[i];
+    }
+  }
+  for (int i=0; i<n; ++i){
+    rank[pos[i]] = i;
+  }
+}
+// End of suffix array algorithm
+
+// Algorithm GetHeight
+// input: A text A and its suffix array Pos
+//    1 for i:=1 to n do
+//    2     Rank[Pos[i]] := i
+//    3 od
+//    4 h:=0
+//    5 for i:=1 to n do
+//    6     if Rank[i] > 1 then
+//    7        k := Pos[Rank[i]-1]
+//    8        while A[i+h] = A[j+h] do
+//    9           h := h+1
+//    10       od
+//    11       Height[Rank[i]] := h
+//    12       if h > 0 then h := h-1 fi
+//    13    fi
+//    14 od
+int height[N];
+// height[i] = length of the longest common prefix of suffix pos[i] and suffix pos[i-1]
+// height[0] = 0
+void getHeight(int n){
+  for (int i=0; i<n; ++i) rank[pos[i]] = i;
+  height[0] = 0;
+  for (int i=0, h=0; i<n; ++i){
+    if (rank[i] > 0){
+      int j = pos[rank[i]-1];
+      while (i + h < n && j + h < n && str[i+h] == str[j+h]) h++;
+      height[rank[i]] = h;
+      if (h > 0) h--;
+    }
+  }
+}
+
+// Gets the longest common prefix from Sx and Sy in a string of lenght n
+// lcp(x,y) = min(lcp(x,x+1), lcp(x+1, x+2), ... , lcp(y-1, y))
+// Runs in O(|x-y|)
+int lcp(int x, int y, int n){
+    if(x > y) return lcp(y,x,n);
+    if(x == y) return n-x;
+    int lc = n+1;
+    for(int i = x+1; i<=y; ++i) lc = min(lc, height[i]);
+    return lc;
+}
+
+
+string s;
+
+// You need a string W that represents the pattern
+int match_prefix(int n){
+    string W; // Fill this outside
+    if(W[0] < s[pos[0]]) return -1; // Is not here!
+    if(W[0] > s[pos[n-1]]) return -1; // Not here too!
+    if(W == s.substr(pos[0])) return pos[0];
+    // Binary search for the W pattern
+    int l = 0, r = n-1, m;
+    while(r-l > 1){
+	m = (l+r)/2;
+	if(W >= s.substr(pos[m]))
+	    l = m;
+	else
+	    r = m;
+    }
+    // r is the i-sth smallest suffix
+    // that means that pos[r] is the actual index
+    if(W != s.substr(pos[r], W.size())) return -1; // not here at all!
+    printf("Matched at %d\n", r);
+    return pos[r];
+}
+
+
+void solve(){
+  int n = s.size();
+  if(n==1){ cout << 0 << endl;  return; }
+  for (int i=0; i<n; ++i) str[i] = s[i];
+  SuffixSort(n);
+  getHeight(n);
+  long long number_of_different_substrings = 0;
+  // If you have the i-th smaller suffix, Si, it's length will be |Si| = n - pos[i]
+  // Now, height[i] stores the number of common letters between Si and Si (s.substr(pos[i]) and s.substr(pos[i-1]))
+  // so, you have |Si| - height[i] different strings from these two suffixes => n - pos[i] - height[i]
+  // for (int i=0; i<n; ++i){      
+  //     number_of_different_substrings += n - pos[i] - height[i];
+  //   }
+  //cout << number_of_different_substrings << endl;
+  
+  // Print the suffix array
+  //   puts("Suffix Array");
+  //   string tmp;
+  //   for(int i=0;i<n;++i){
+  //       tmp = s.substr(pos[i]);
+  //       printf("pos[%d] = %2d \t suffix = %s \t height[%d] = %d\n",i,pos[i],tmp.c_str(), i, height[i]);
+  //   }
+  
+  // Number of substrings that appear at least twice in the text
+  // The trick is that all 'spare' substrings that can give us 
+  // Lcp(i - 1, i) can be obtained by Lcp(i - 2, i - 1) 
+  // due to the ordered nature of our array. And the overall answer is:
+  // Lcp(0, 1) + Sum(max[0, Lcp(i, i - 1) - Lcp(i - 2, i - 1)]), 2 <= i < n
+  int cnt = height[1];
+  for(int i=2;i<n;++i){
+      cnt += max(0, height[i] - height[i-1]);
+  }
+  cout << cnt << endl;
+}
+
+int main(){
+  int t;
+  while (cin >> s){
+      if(s=="*") break;
+      solve();
+  }
+  return 0;
+}
+
