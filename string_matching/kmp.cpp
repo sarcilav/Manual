fe933e76af2a933c13a969e7d1ae865296756ef0
Nicolás Hock Isaza <nhocki@gmail.com>
2010-10-06 19:19:33 -0500

    kmp failure function

--- string_matching/kmp.cpp
+++ string_matching/kmp.cpp
@@ -11,45 +11,42 @@ using namespace std;
 #define foreach(x, v) for (typeof (v).begin() x = (v).begin(); x != (v).end(); ++x)
 
 // Computes the jumping function
-vector<int> kmp_table(string &P){
-    int n = P.size();
-    vector<int> T(n+1);
-    T[0] = -1; T[1] = 0;
-    for(int pos = 2, cnd = 0; pos < n; ++pos){
-	if(P[pos -1] == P[cnd]){ T[pos] = ++cnd;}
-	else if(cnd > 0) { cnd = T[cnd]; --pos; }
-	else T[pos] = 0;
-    }
-    return T;
-}
-
-// Looking for W in S
-void kmp_search(string &S, string &W){
-    //printf("Looking for  %s  in  %s\n", W.c_str(), S.c_str());
-    set<int>s;
-    int m = 0, i = 0, n = S.size(), w = W.size();
-    vector<int> T = kmp_table(W);
-    while( m + i < n){
-	if(W[i] == S[m+i]){
-	    if(++i == w){s.insert(m); m--; }//printf("There's a match at %d\n", m); m--;}
-	}
-	else{
-	    m += i - T[i];
-	    i = (T[i] > -1) ? T[i] : 0;
-	}
-    }
-    printf("%d\n", s.size());
-    foreach(i,s){ printf("%d ", *(i)); }
-    puts("");
-    return ;
+int kmp_table(string &P){
+  int i = 1, j = 0;
+  int m = P.size();
+  vector<int> f(m);
+  f[0] = 0;
+  while( i < m ){
+    if(P[j] == P[i])
+      f[i++] = ++j;
+    else if(j>0)
+      j = f[j-1];
+    else
+      f[i++] = 0;
+  }
+  return f.back();
 }
 
 int main(){
-    //string T = "CABBCABABABA";
-    //string P = "ABA";
-    //string P = "ababababca";
-    string T,P;
-    cin >> P >> T;
-    kmp_search(T, P);
-    return 0;
+  string P;
+  int k;
+  while(scanf("%d", &k) == 1){
+    if(k < 1) break;
+    cin >> P;
+    int N = P.size();
+    if( N > k) puts("0");
+    else if(N == k) puts("1");
+    else if(N == 1) printf("%d\n", k);
+    else{
+      int overlap = kmp_table(P);
+      int ans = 0;
+      if(overlap > 0){
+        ans++;
+        k-=N;
+      }
+      ans += ( k / (N-overlap) );
+      cout << ans << endl;
+    }
+  }
+  return 0;
 }
